0bef5f5bafedb1e447f7dc3335d0e663accd0616
lim <dntksql1010@naver.com>
2025-06-30 17:22:56 +0900

    add: todo title 추가

---
+++ components/TodoTitle.js
@@ -0,0 +1,10 @@
+function TodoTitle() {
+  const Title = document.createElement('h1');
+  Title.className = 'todo-title';
+
+  Title.innerHTML = `TODO APP`;
+
+  return Title;
+}
+
+export default TodoTitle;
